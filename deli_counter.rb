d93a7afda266eabf5d7f890ab2f973fcdb9b11d7
Winnie Mendoza <wrm956@gmail.com>
2022-08-08 16:34:10 -0400

    commit tree 9782e03129a768cb163a8b956f85de87ef155aa2

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+# katz_deli = []
+
+def line(queue)
+    return puts "The line is currently empty." if queue.empty?
+
+    customers = queue.map.with_index(1) do |name, index|
+        "#{index}. #{name}"
+    end
+    
+    puts "The line is currently: #{customers.join(' ')}"
+end
+
+def take_a_number(queue, name)
+    queue << name
+    puts "Welcome, #{name}. You are number #{queue.length} in line."
+
+end
+
+def now_serving(queue)
+    return puts "There is nobody waiting to be served!" if queue.empty?
+    puts "Currently serving #{queue.first}."
+    queue.shift
+end
